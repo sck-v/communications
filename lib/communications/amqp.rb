a39c2182abc52f8b0e2febcf99bd9aa77e951e4e
Ivan Kryak <kryak.iv@gmail.com>
2014-11-26 16:11:26 +0500

    Wrap publishing in next_tick method

--- lib/communications/amqp.rb
+++ lib/communications/amqp.rb
@@ -7,15 +7,6 @@ module Communications
   class Amqp
     include Singleton
 
-    def publish(route, message)
-      AMQP::Utilities::EventLoopHelper.run do
-        message = message.to_json if message.respond_to?(:to_json)
-
-        exchange = channel.default_exchange
-        exchange.publish(message, routing_key: Configuration.with_channel_prefix(route), content_type: 'application/json')
-      end
-    end
-
     def channel
       AMQP::Channel.new(connection)
     end
--- lib/communications/publisher.rb
+++ lib/communications/publisher.rb
@@ -5,8 +5,15 @@ module Communications
 
     class << self
       def publish(event, message)
-        Communications::Amqp.instance.publish(event, message)
-        event
+        AMQP::Utilities::EventLoopHelper.run do
+          EM.next_tick do
+            message = message.to_json if message.respond_to?(:to_json)
+
+            channel = Communications::Amqp.instance.channel
+            exchange = channel.default_exchange
+            exchange.publish(message, routing_key: Configuration.with_channel_prefix(event), content_type: 'application/json')
+          end
+        end
       end
     end
   end
